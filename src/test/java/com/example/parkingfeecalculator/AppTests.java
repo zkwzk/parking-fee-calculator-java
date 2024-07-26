e77da40bae91d665f00158e52f459a70558a3e8d
Zhengkun Wang <zkwang@thoughtworks.com>
2024-07-26 10:30:54 +0800

    [ZK] init commit

---
+++ src/test/java/com/example/parkingfeecalculator/AppTests.java
@@ -0,0 +1,15 @@
+package com.example.parkingfeecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
