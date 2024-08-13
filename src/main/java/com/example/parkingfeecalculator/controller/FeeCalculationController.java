70114eaa8a55de66cb7d83824460973ade5f3014
Zhengkun Wang <zkwang@thoughtworks.com>
2024-08-13 12:07:05 +0800

    [ZK] added example value for swagger

--- src/main/java/com/example/parkingfeecalculator/controller/FeeCalculationController.java
+++ src/main/java/com/example/parkingfeecalculator/controller/FeeCalculationController.java
@@ -18,6 +18,7 @@ import com.example.parkingfeecalculator.model.CarParkConfig;
 import com.example.parkingfeecalculator.model.CarParkFee;
 import com.example.parkingfeecalculator.model.VehicleType;
 import com.example.parkingfeecalculator.service.FeeCalculationService;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @RestController
 @RequestMapping("/api/feecalculator")
@@ -27,9 +28,9 @@ public class FeeCalculationController {
     
     @GetMapping("/getLowestCarpark")
     public ResponseEntity<?> getLowestCarpark(
-        @RequestParam(value="startTime",required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime, 
-        @RequestParam(value="endTime", required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime, 
-        @RequestParam(value="vehicleType", required = true) String vehicleType) {
+        @Parameter(description = "Start time in ISO format", example = "2024-03-20T10:00:00") @RequestParam(value="startTime",required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime, 
+        @Parameter(description = "End time in ISO format", example = "2024-03-22T12:00:00") @RequestParam(value="endTime", required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime, 
+        @Parameter(description = "Vehicle Type, Car or Motorcycle", example = "Car") @RequestParam(value="vehicleType", required = true) String vehicleType) {
         List<CarParkFee> carParkFees = new ArrayList<CarParkFee>();
         if(vehicleType.toLowerCase().equals("car")) {
             for (CarPark carPark : CarParkConfig.CarParks) {
