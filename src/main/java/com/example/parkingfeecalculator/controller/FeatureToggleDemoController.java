f450010337ff3c18c1ad75351714a66e2dacb947
Zhengkun Wang <zkwang@thoughtworks.com>
2024-08-30 10:05:55 +0800

    [ZK] setup the task for feature toggle

--- src/main/java/com/example/parkingfeecalculator/controller/FeatureToggleDemoController.java
+++ src/main/java/com/example/parkingfeecalculator/controller/FeatureToggleDemoController.java
@@ -18,10 +18,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class FeatureToggleDemoController {
     // change toggle online: https://www.npoint.io/docs/0752974a4a6668c526af
     private final String toggleUrl = "https://api.npoint.io/0752974a4a6668c526af";
+    // step 3: fill in your toggle url here, e.g. "https://api.npoint.io/0752974a4a6668c526af";
+    private final String testToggleUrl = "";
+    // step 3: fill in your toggle name here, e.g. "toggle";
+    private final String testToggleName = "";
     @GetMapping("/hello")
     public ResponseEntity<?> hello() {
         try {
-            if(isFeatureEnabled()) {
+            if(isFeatureEnabled(toggleUrl, "toggle")) {
                 return ResponseEntity.ok().body("Hello, World!");
             }
 
@@ -31,12 +35,27 @@ public class FeatureToggleDemoController {
         }
     }
 
-    private boolean isFeatureEnabled() throws Exception {
+    @GetMapping("/testToggle")
+    public ResponseEntity<?> testToggle() {
+        try {
+            if(isFeatureEnabled(testToggleUrl, testToggleName)) {
+                // step 4: when toggle on
+                return ResponseEntity.ok().body("Hello, World!");
+            }
+
+            // step 4: when toggle off
+            return ResponseEntity.status(403).body("Feature is disabled");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error occurred while checking feature toggle");
+        }
+    }
+
+    private boolean isFeatureEnabled(String url, String toggleName) throws Exception {
         try {
             HttpClient client = HttpClient.newHttpClient();
 
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(toggleUrl))
+                    .uri(URI.create(url))
                     .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -44,7 +63,7 @@ public class FeatureToggleDemoController {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode jsonNode = objectMapper.readTree(response.body());
 
-            return jsonNode.get("toggle").asBoolean();
+            return jsonNode.get(toggleName).asBoolean();
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
